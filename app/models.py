0d86a6e1aad6993fd838665904fb2bae8f281516
gideon098 <gjp284@nyu.edu>
2018-11-25 16:12:30 -0500

    Fix indentation

--- app/models.py
+++ app/models.py
@@ -74,13 +74,13 @@ class Payment(db.Model):
         db.session.delete(self)
         db.session.commit()
 
-	def set_default(self):
-		""" Sets a Payment as the default """
-		self.default_payment_type = True
+    def set_default(self):
+        """ Sets a Payment as the default """
+        self.default_payment_type = True
 	
-	def unset_default(self):
-		""" Disables the default status for a Payment """
-		self.default_payment_type = False
+    def unset_default(self):
+        """ Disables the default status for a Payment """
+        self.default_payment_type = False
 
     def serialize(self):
         """ Serializes a Payment into a dictionary """
