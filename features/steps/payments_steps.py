10aa15ce571659a8fb9fcffa0676fa7d65dcbe0c
varsh27 <33126448+varsh27@users.noreply.github.com>
2018-12-12 17:08:08 -0500

    Update payments_steps.py

--- features/steps/payments_steps.py
+++ features/steps/payments_steps.py
@@ -12,8 +12,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 
 WAIT_SECONDS = 60
-BASE_URL = getenv('BASE_URL', 'http://localhost:5000')
-
+# BASE_URL = getenv('BASE_URL', 'http://localhost:5000')
+BASE_URL = os.getenv('BASE_URL', 'https://nyu-payment-service-f18.mybluemix.net/')
 
 @given('the following payments')
 def step_impl(context):
