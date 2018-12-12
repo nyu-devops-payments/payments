f9757f518f167226d5828b9edeee28ed24ce3512
Gideon Popkin <gjp284@nyu.edu>
2018-12-12 13:19:47 -0500

    Fix service.py and UI

--- app/static/js/rest_api.js
+++ app/static/js/rest_api.js
@@ -45,12 +45,12 @@ $(function () {
 
     /// Clears all form fields
     function clear_form_data() {
-      $("#payment_id").val("");
-      $("#payment_customer_id").val("");
-      $("#payment_order_id").val("");
-      $("#payment_payment_method_type").val("");
-      $("#payment_payment_status").val("");
-      $("#payment_default_payment_type").val("");
+        $("#payment_id").val("");
+        $("#payment_customer_id").val("");
+        $("#payment_order_id").val("");
+        $("#payment_payment_method_type").val("");
+        $("#payment_payment_status").val("");
+        $("#payment_default_payment_type").val("");
     }
 
     // Updates the flash message area
@@ -73,27 +73,27 @@ $(function () {
         var default_payment_type = $("#payment_default_payment_type").val() == "false";
 
         var data = {
-          "id" : id,
-          "customer_id" : customer_id,
-          "order_id" : order_id,
-          "payment_method_type" : "DEBIT",
-          "payment_status" : payment_status,
-          "default_payment_type" : default_payment_type
+            "id": id,
+            "customer_id": customer_id,
+            "order_id": order_id,
+            "payment_method_type": "DEBIT",
+            "payment_status": payment_status,
+            "default_payment_type": default_payment_type
         };
 
         var ajax = $.ajax({
             type: "POST",
             url: "/payments",
-            contentType:"application/json",
+            contentType: "application/json",
             data: JSON.stringify(data),
         });
 
-        ajax.done(function(res){
+        ajax.done(function (res) {
             update_form_data(res)
             flash_message("Success - Payment Added!")
         });
 
-        ajax.fail(function(res){
+        ajax.fail(function (res) {
             flash_message(res.responseJSON.message)
         });
     });
@@ -113,27 +113,27 @@ $(function () {
         var default_payment_type = $("#payment_default_payment_type").val() == "false";
 
         var data = {
-          "id" : id,
-          "customer_id" : customer_id,
-          "order_id" : order_id,
-          "payment_method_type" : payment_method_type,
-          "payment_status" :payment_status,
-          "default_payment_type" : default_payment_type
+            "id": id,
+            "customer_id": customer_id,
+            "order_id": order_id,
+            "payment_method_type": payment_method_type,
+            "payment_status": payment_status,
+            "default_payment_type": default_payment_type
         };
 
         var ajax = $.ajax({
-                type: "PUT",
-                url: "/payments/" + id,
-                contentType:"application/json",
-                data: JSON.stringify(data)
-            })
+            type: "PUT",
+            url: "/payments/" + id,
+            contentType: "application/json",
+            data: JSON.stringify(data)
+        })
 
-        ajax.done(function(res){
+        ajax.done(function (res) {
             update_form_data(res)
             flash_message("Success")
         });
 
-        ajax.fail(function(res){
+        ajax.fail(function (res) {
             flash_message(res.responseJSON.message)
         });
 
@@ -150,17 +150,17 @@ $(function () {
         var ajax = $.ajax({
             type: "GET",
             url: "/payments/" + id,
-            contentType:"application/json",
+            contentType: "application/json",
             data: ''
         })
 
-        ajax.done(function(res){
+        ajax.done(function (res) {
             //alert(res.toSource())
             update_form_data(res)
             flash_message("Success!!")
         });
 
-        ajax.fail(function(res){
+        ajax.fail(function (res) {
             clear_form_data()
             flash_message(res.responseJSON.message)
         });
@@ -178,16 +178,16 @@ $(function () {
         var ajax = $.ajax({
             type: "DELETE",
             url: "/payments/" + id,
-            contentType:"application/json",
+            contentType: "application/json",
             data: '',
         })
 
-        ajax.done(function(res){
+        ajax.done(function (res) {
             clear_form_data()
             flash_message("Payment with ID [" + res.id + "] has been Deleted!")
         });
 
-        ajax.fail(function(res){
+        ajax.fail(function (res) {
             flash_message("Server error!")
         });
     });
@@ -214,67 +214,71 @@ $(function () {
         var payment_status = $("#payment_payment_status").val();
         var default_payment_type = $("#payment_default_payment_type").val() == "false";
 
-        var queryString = ""
+        var queryString = "";
 
 
         if (id) {
-            queryString += 'id=' + id
+            queryString += 'id=' + id;
         }
         if (customer_id) {
-            queryString += 'customer_id=' + customer_id
+            queryString += 'customer_id=' + customer_id;
         }
         if (order_id) {
-            queryString += 'order_id' + order_id
+            queryString += 'order_id' + order_id;
         }
         if (payment_method_type) {
-            queryString += 'payment_method_type' + payment_method_type
+            queryString += 'payment_method_type' + payment_method_type;
         }
         if (payment_status) {
-            queryString += 'payment_status' + payment_status
+            queryString += 'payment_status' + payment_status;
         }
         if (default_payment_type) {
             if (queryString.length > 0) {
-                queryString += '&default_payment_type=' + default_payment_type
+                queryString += '&default_payment_type=' + default_payment_type;
             } else {
-                queryString += 'default_payment_type=' + default_payment_type
+                queryString += 'default_payment_type=' + default_payment_type;
             }
         }
 
         var ajax = $.ajax({
             type: "GET",
             url: "/payments?" + queryString,
-            contentType:"application/json",
+            contentType: "application/json",
             data: ''
-        })
-
-        ajax.done(function(res){
-            //alert(res.toSource())
-            $("#search_results").empty();
-            $("#search_results").append('<table class="table-striped">');
-            var header = '<tr>'
-            header += '<th style="width:10%">ID</th>'
-            header += '<th style="width:40%">Customer ID</th>'
-            header += '<th style="width:40%">Order ID</th>'
-            header += '<th style="width:10%">Payment Method Type</th>'
-            header += '<th style="width:10%">Payment Status</th>'
-            header += '<th style="width:10%">Default Payment Type</th>'
-
-            $("#search_results").append(header);
-            for(var i = 0; i < res.length; i++) {
-                payment = res[i];
-                var row = "<tr><td>"+payment.id+"</td><td>"+payment.customer_id+"</td><td>"+payment.order_id+"</td><td>"+payment.payment_method_type+"</td></tr>"+"<tr><td>"+payment.payment_status+"</td><td>"+"<tr><td>"+payment.default_payment_type+"</td><td>";
-                $("#search_results").append(row);
-            }
-
-            $("#search_results").append('</table>');
-
-            flash_message("Success")
         });
 
-        ajax.fail(function(res){
-            flash_message(res.responseJSON.message)
+        ajax.done(function (res) {
+            //alert(res.toSource())
+            for(var i = 0; i < res.length; i++) {
+
+            var html = '';
+            html += '<table class="table-striped pad-10">';
+            html += '<tr>';
+            html += '<th style="width:10%">ID</th>';
+            html += '<th style="width:40%">Customer ID</th>';
+            html += '<th style="width:40%">Order ID</th>';
+            html += '<th style="width:10%">Payment Method Type</th>';
+            html += '<th style="width:10%">Payment Status</th>';
+            html += '<th style="width:10%">Default Payment Type</th>';
+            html += '</tr>';
+
+            for (var i = 0; i < res.length; i++) {
+                payment = res[i];
+                html += "<tr><td>" + payment.id + "</td><td>" + payment.customer_id + "</td><td>" + payment.order_id + "</td><td>" + payment.payment_method_type + "</td><td>" + payment.payment_status + "</td><td>" + payment.default_payment_type + "</td></tr>";
+            }
+
+            html += '</table>';
+
+            $("#search_results").empty();
+            $("#search_results").append(html);
+
+            flash_message("Success");
+        });
+
+        ajax.fail(function (res) {
+            flash_message(res.responseJSON.message);
         });
 
     });
 
-})
+});
